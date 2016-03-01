7db6a5382b365acf6a9da847e89f0e3f7cc527a0
richard <richard.l.white@gmail.com>
2016-03-01 17:15:51 -0500

    Video implementation, slug'd names, etc

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -14,7 +14,15 @@
 import "phoenix_html"
 
 // Import local files
-//
+import Player from "./player"
+let video = document.getElementById("video")
+
+if(video) {
+  Player.init(video.id, video.getAttribute("data-player-id"), () => {
+    console.log("player ready!")
+  })
+}
+
 // Local files can be imported directly using relative
 // paths "./socket" or full ones "web/static/js/socket".
 
