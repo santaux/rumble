cb70afd570680060611864fb8d5285810d524afb
richard <richard.l.white@gmail.com>
2016-03-02 14:49:10 -0500

    Converting to umbrella application

--- apps/info_sys/lib/info_sys.ex
+++ apps/info_sys/lib/info_sys.ex
@@ -1,9 +1,14 @@
-defmodule Rumbl.InfoSys do
-  @backends [Rumbl.InfoSys.Wolfram]
+defmodule InfoSys do
+  use Application
+  @backends [InfoSys.Wolfram]
 
   defmodule Result do
     defstruct score: 0, text: nil, url: nil, backend: nil
   end
+
+  def start(_type, _args) do
+  InfoSys.Supervisor.start_link()
+  end  
 
   def start_link(backend, query, query_ref, owner, limit) do
     backend.start_link(query, query_ref, owner, limit)
@@ -23,7 +28,7 @@ defmodule Rumbl.InfoSys do
   defp spawn_query(backend, query, limit) do
     query_ref = make_ref()
     opts = [backend, query, query_ref, self(), limit]
-    {:ok, pid} = Supervisor.start_child(Rumbl.InfoSys.Supervisor, opts)
+    {:ok, pid} = Supervisor.start_child(InfoSys.Supervisor, opts)
     monitor_ref = Process.monitor(pid)
     {pid, monitor_ref, query_ref}
   end
